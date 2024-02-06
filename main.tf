e4a89516bafd3b90a30d9524a35efa8106a0a0cc
Femi Oyebisi <femioyebisi@Femis-MBP.lan>
2024-02-06 11:25:35 -0600

    added security group and fixed userdata

--- main.tf
+++ main.tf
@@ -45,10 +45,36 @@ resource "aws_instance" "nginx_server" {
   ami           = var.ami_id
   instance_type = "t3.micro"
 
-  user_data_base64 = base64encode("./userdata.sh")
+  user_data = file("./userdata.sh")
   tags = {
     Name = "NGINX Server"
   }
+}
+
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow http inbound traffic from port 80"
+  vpc_id      = aws_vpc.main.id
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http_ipv4" {
+  security_group_id = aws_security_group.allow_http.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_http.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
+  security_group_id = aws_security_group.allow_http.id
+  cidr_ipv6         = "::/0"
+  ip_protocol       = "-1"
 }
 
 output "ami_id" {
